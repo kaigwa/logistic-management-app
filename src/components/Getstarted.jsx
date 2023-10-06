ed62586bb3201a53dbee13f596f95c0d737137c1
kaigwa <kaigwaedwin@gmail.com>
2023-10-06 10:03:16 +0300

    first commit

---
+++ src/components/Getstarted.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Getstarted = (props) => {
+  return (
+    <div>
+      <h4>Logistic Management</h4>
+      <button>Waste Management</button>
+      <button>Transport Management</button>
+    </div>
+  )
+};
+
+export default Getstarted;
